4f26df35fc3d2e672f3167d5cac32ba6546e87e1
YoungMin KIM <47775179+winston1214@users.noreply.github.com>
2022-01-08 21:17:25 +0900

    Update keypoint_process.py

--- utils/keypoint_process.py
+++ utils/keypoint_process.py
@@ -43,25 +43,36 @@ def video_sampling(data,video_size):
     return X_train
 
 
-import numpy as np
-from tqdm.notebook import tqdm
-import re
 
 def video_sampling2(data): # original video에서 hand video 부분만 random하게 frame 증강
     np.random.seed(42)
     X_train=np.array([])
     num_ls = []
     max_frame_num = 0
+    min_frame_num = []
+    tmp = []
     hand_frame_ls = np.array([])
+    video_name = [] # video 밀리는 경우 방지(frame이 0번째부터 있지 않을 수 있음)
     for idx,i in tqdm(enumerate(data)):
         num2 = i.split('_')[3]
         num2 = int(re.sub(".jpg", "", num2))
         num_ls.append(int(num2))
-        
+        video_name.append(i)
+        try:
+            if num_ls[idx+1] - num_ls[idx]<0:
+                tmp.append(num2)
+                min_frame_num.append([min(tmp)]*len(tmp))
+            else:
+                tmp.append(num2)
+                
+        except:
+            tmp.append(num2)
+            min_frame_num.append([min(tmp)]*len(tmp))
         if max_frame_num < num2: # 최댓값 찾기
             max_frame_num = num2
-      
-
+            
+    min_frame_num = sum(min_frame_num,[])
+    max_frame_num += 1 # 0부터 시작해서
     video_hand_idx = []
     print('max_frame_num', max_frame_num)
     video_frame = np.array([])
@@ -81,36 +92,48 @@ def video_sampling2(data): # original video에서 hand video 부분만 random하
             hand_frame_ls = np.append(hand_frame_ls,dt[:,:2].flatten())
             
             
+        try:
+            if num_ls[idx+1]-num_ls[idx] < 0:
+                video_frame = np.append(video_frame,dt[:,:2])
+                hand_frame_ls = hand_frame_ls.reshape(-1,246)
+                video_frame = video_frame.reshape(-1,246)
 
-        if num_ls[idx+1] == 0:
+
+                if video_frame.shape[0] < max_frame_num: # 비디오프레임이 최대 비디오프레임보다 적을 때
+                    if min_frame_num[idx] != 0: # 시작 프레임이 0이 아닐 때
+                        start_number = min_frame_num[idx]
+                        print(start_number,idx)
+                        random_choice_hand = np.random.choice(video_hand_idx,max_frame_num - num_ls[idx]+start_number-1)
+                        for random_idx,h in enumerate(random_choice_hand):
+                            insert_num = h+random_idx-start_number
+                            video_frame = np.insert(video_frame,insert_num,video_frame[insert_num],axis=0) # 손 부분만 증강
+                    else:
+                        random_choice_hand = np.random.choice(video_hand_idx,max_frame_num - num_ls[idx]-1) # 중복 허용
+                        random_choice_hand.sort()
+                        for random_idx,h in enumerate(random_choice_hand):
+
+                            video_frame = np.insert(video_frame,h+random_idx,video_frame[h+random_idx],axis=0) # 손 부분만 증강
+
+    #                 print(video_frame.shape)
+    #                 print(len(random_choice_hand))
+
+
+
+    #             print(video_frame.shape)
+                if video_frame.shape[0] != max_frame_num: # error check
+                    print(list(data.keys())[idx]) # error check
+                    print(video_frame.shape) # error check
+                X_train = np.append(X_train,video_frame).reshape(-1,246)
+
+                video_frame = np.array([])
+                hand_frame_ls = np.array([])
+                video_hand_idx = []
+            else:
+                video_frame = np.append(video_frame,dt[:,:2])
+        except:
             video_frame = np.append(video_frame,dt[:,:2])
-            hand_frame_ls = hand_frame_ls.reshape(-1,246)
-            video_frame = video_frame.reshape(-1,246)
             
-
             
-            if video_frame.shape[0] < max_frame_num: # 비디오프레임이 최대 비디오프레임보다 적을 때
-                
-                random_choice_hand = np.random.choice(video_hand_idx,max_frame_num - num_ls[idx]-1) # 중복 허용
-                random_choice_hand.sort()
-                
-
-            
-                for random_idx,h in enumerate(random_choice_hand):
-
-                    video_frame = np.insert(video_frame,h+random_idx,video_frame[h+random_idx],axis=0) # 손 부분만 증강
-
-#             if video_frame.shape[0] != max_frame_num: # error check
-#                 print(list(data.keys())[idx])
-#                 print(video_frame.shape)
-            X_train = np.append(X_train,video_frame).reshape(-1,246)
-            
-            video_frame = np.array([])
-            hand_frame_ls = np.array([])
-            video_hand_idx = []
-        else:
-            video_frame = np.append(video_frame,dt[:,:2])
-            print(video_frame.shape)
     X_train = np.array(X_train).reshape(-1,max_frame_num,246)
             
     return X_train
