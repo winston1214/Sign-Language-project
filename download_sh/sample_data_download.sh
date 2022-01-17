68907c777d32b059fc9bd8632859935e54ed04c5
winston1214 <winston1214@naver.com>
2022-01-17 20:55:19 +0900

    commit

--- download_sh/sample_data_download.sh
+++ download_sh/sample_data_download.sh
@@ -1,2 +1,2 @@
-./gdown.pl 'https://drive.google.com/file/d/1xNvciV830HPNEz3tlsuNvcCKP6rqKnYG' sample_video1.mp4
+./gdown.pl 'https://drive.google.com/file/d/138voIrUm4-GXIbz0ViHzwoeXLD5mg__C' sample_video1.mp4
 ./gdown.pl 'https://drive.google.com/file/d/1-gFb3MPjdvqLnXMZRFhnUozKKsa7iU_c' sample_video2.mp4
