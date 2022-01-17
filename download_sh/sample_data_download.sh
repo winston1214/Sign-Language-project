4451d7b626b72a9d87a7ae2c642f7d85d2653607
winston1214 <winston1214@naver.com>
2022-01-17 19:44:08 +0900

    commit

---
+++ download_sh/sample_data_download.sh
@@ -0,0 +1,2 @@
+./gdown.pl 'https://drive.google.com/file/d/1xNvciV830HPNEz3tlsuNvcCKP6rqKnYG' sample_video1.mp4
+./gdown.pl 'https://drive.google.com/file/d/1-gFb3MPjdvqLnXMZRFhnUozKKsa7iU_c' sample_video2.mp4
--- download_sh/smaple_data_download.sh
+++
@@ -1,2 +0,0 @@
-./gdown.pl 'https://drive.google.com/file/d/1xNvciV830HPNEz3tlsuNvcCKP6rqKnYG' sample_video1
-./gdown.pl 'https://drive.google.com/file/d/1-gFb3MPjdvqLnXMZRFhnUozKKsa7iU_c' sample_video2
