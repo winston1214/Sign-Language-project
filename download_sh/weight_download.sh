1c52395f37caa1edf2f21a2b1d83a4758ad5598a
winston1214 <winston1214@naver.com>
2022-01-17 19:29:40 +0900

    download sh

---
+++ download_sh/weight_download.sh
@@ -0,0 +1,6 @@
+wget https://raw.github.com/circulosmeos/gdown.pl/master/gdown.pl
+chmod u+x gdown.pl
+mkdir detector/yolo/data
+./gdown.pl 'https://drive.google.com/file/d/1fwl4Hpeuu5fneIGWXsUJK0RBvbMDpISP' detector/yolo/data/yolov3-spp.weights
+./gdown.pl 'https://drive.google.com/file/d/1nTJ04JBuETWjj-pAF37tR-JunVEW9H46' pretrained_models/halpe136_fast_res50_256x192.pth
+./gdown.pl 'https://drive.google.com/file/d/1lpPNIutVy1a4XleT4GYl2rVAI2u3NI3a' best_GRU_TUNNING.pt
