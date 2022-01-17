c5694c5961cbd47f95dad5f71a3e1db847134865
winston1214 <winston1214@naver.com>
2022-01-17 19:11:14 +0900

    sample data download

---
+++ sample_data/smaple_data_download.sh
@@ -0,0 +1,2 @@
+./gdown.pl 'https://drive.google.com/file/d/1xNvciV830HPNEz3tlsuNvcCKP6rqKnYG' sample_video1
+./gdown.pl 'https://drive.google.com/file/d/1-gFb3MPjdvqLnXMZRFhnUozKKsa7iU_c' sample_video2
