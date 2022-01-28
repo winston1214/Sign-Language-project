026fcd39a8a265b01d48bf2d777ef3b54d7d49f0
unknown <winston1214@naver.com>
2022-01-28 19:00:16 +0900

    keti

--- utils/keypoint_process.py
+++ utils/keypoint_process.py
@@ -1,47 +1,123 @@
 import numpy as np
 from tqdm.notebook import tqdm
 import re
+import math
 
-def video_sampling(data,video_size):
-    X_train=[]
-    num_ls = [0]
-
-    hand_frame_ls = np.array([])
-
+def keti(data): # 전체 frame에 대해서 random하게 증강
+    np.random.seed(42)
+    X_train=np.array([])
+    num_ls = []
+    max_frame_num = 0
+    min_frame_num = []
+    video_set = set()
+    minimum = 1e+5
+    video_dic = {}
+    video_name = [] # video 밀리는 경우 방지(frame이 0번째부터 있지 않을 수 있음)
+    for idx,i in tqdm(enumerate(data)):
+        # print(idx) # 반복 횟수 1
+        # print(i) # video name : KETI_SL_0000035402_0.jpg
+        num2 = i.split('_')[3] # 0
+        num2 = int(re.sub(".jpg", "", num2))
+         # print(num2) # frame number
+        num_ls.append(int(num2))   # num_ls : frame number들의 list   num_ls = [0]
+        video_name.append(i) # video name : KETI_SL_0000035402_90.jpg
+        video_set.add('_'.join(i.split('.')[0].split('_')[:-1]))
 
     for idx,i in tqdm(enumerate(data)):
-        num2 = i.split('_')[3]
-        num2 = re.sub(".jpg", "", num2)
-        num_ls.append(int(num2))
 
+        num2 = i.split('_')[3] # 0
+        num2 = int(re.sub(".jpg", "", num2))
+
+        if max_frame_num < num2: # 최댓값 frame 찾기
+            max_frame_num = num2
+            
+        try: 
+            if num_ls[idx+1] - num_ls[idx] <0: # num_ls : frame number들의 list # 123->1로 frame number가 넘어갈때, 1<123이므로 1-123<0
+                min_frame_num.append(minimum) 
+                minimum = 1e+5
+            else:
+                if minimum > num2:
+                    minimum = num2
+        except: # 끝
+            if minimum > num2:
+                minimum = num2
+            min_frame_num.append(minimum)
+    video_set = sorted(list(video_set))
+    for k,v in zip(video_set,min_frame_num):
+        video_dic[k] = v
+
+    max_frame_num += 1 # 0부터 시작해서
+
+    print('max_frame_num', max_frame_num)
+    max_frame_num = 50
+    n = max_frame_num
+    
+    video_frame = np.array([])
+
+    for idx,i in tqdm(enumerate(data)):
+        num2 = num_ls[idx]
         dt = data[i]['keypoints']
         dt = np.array(dt).reshape(123,3)
-
+        dt = np.delete(dt,range(13,81),axis=0) # 얼굴 키포인트 제외
         mean_x,std_x = np.mean(dt[:,0]),np.std(dt[:,0]) # noramlization
         mean_y,std_y = np.mean(dt[:,1]),np.std(dt[:,1]) # normalization
         normalization_x, normalization_y = (dt[:,0] - mean_x)/std_x, (dt[:,1] - mean_y)/std_y # normalization
         dt[:,0] = normalization_x # normalization
         dt[:,1] = normalization_y # normalization
 
-        if set([i for i in range(81,124)]) & set(np.where(dt[:,2]>0.5)[0]): # 손이 탐지된 frame
+            
+        try:
+            if num_ls[idx+1]-num_ls[idx] < 0: # 비디오 바뀔 때
+                video_frame = np.append(video_frame,dt[:,:2])
+#                 hand_frame_ls = hand_frame_ls.reshape(-1,110)
+                video_frame = video_frame.reshape(-1,110)
+                
+                if video_name[idx] == 'KETI_SL_0000041984_173.jpg':
+                    avg = np.mean([video_frame[100],video_frame[101]],axis=0)
+                    video_frame = np.insert(video_frame,100,avg,axis=0)
+                    video_frame = video_frame.reshape(-1,110)
+                l = num_ls[idx]
+                z = math.floor(l/(n-1))
+                y = math.floor((l-z*(n-1))/2) # start point
+                r = np.random.randint(1,z+1,n)
+                baseline = []
 
-            hand_frame_ls = np.append(hand_frame_ls,dt[:,:2].flatten())
+                for i in range(n):
+                    baseline.append(y+i*z)
+                select_frame = np.array(baseline) + r
+                if select_frame[-1] >= len(video_frame):
+                    select_frame[-1] = len(video_frame)-1
+                video_frame = video_frame[select_frame]
+                X_train = np.append(X_train,video_frame).reshape(-1,110)
 
-        if num_ls[idx+1] - num_ls[idx] <0 or idx == len(data)-1: # 동영상이 바뀔 때
-            hand_frame_ls = hand_frame_ls.reshape(-1,246)
-            if len(hand_frame_ls)<video_size: # 손 탐지된 횟수가 정한 video 개수보다 적을 때
-                end_frame = np.array([hand_frame_ls[-1]]) # 끝 프레임
-                tmp = np.repeat(end_frame,video_size - len(hand_frame_ls),axis=0) # 끝 프레임 반복
+                video_frame = np.array([])
 
-                hand_frame_ls = np.append(hand_frame_ls,tmp).reshape(-1,246)
-            elif len(hand_frame_ls) > video_size:
-                hand_frame_ls = hand_frame_ls[:video_size]
+            else:
+                video_frame = np.append(video_frame,dt[:,:2])
+        except:
+            video_frame = np.append(video_frame,dt[:,:2])
+            video_frame = video_frame.reshape(-1,110)
+            l = num_ls[idx]
+            z = math.floor(l/(n-1))
+            y = math.floor((l-z*(n-1))/2) # start point
+            r = np.random.randint(1,z,n)
+            baseline = []
 
-            X_train.append(hand_frame_ls.tolist())
-            hand_frame_ls = np.array([])
-    X_train = np.array(X_train).reshape(-1,video_size,246)
+            for i in range(n):
+                baseline.append(y+i*z)
+            select_frame = np.array(baseline) + r
+            if select_frame[-1] >= len(video_frame):
+                select_frame[-1] = len(video_frame)-1
+            video_frame = video_frame[select_frame]
+
+            X_train = np.append(X_train,video_frame).reshape(-1,110)
+
+            video_frame = np.array([])
+
+            
+    X_train = np.array(X_train).reshape(-1,max_frame_num,110)
+    
     return X_train
-
 
 
 def video_sampling2(data): # original video에서 hand video 부분만 random하게 frame 증강
